8b3ca78252ff701c874835aaea3cc1d9e889e408
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-11-27 17:11:42 +0100

    Separate macro declaration from implentation.
    This commit removes the unification of macro implementation and
    declaration for the scalac engine. The unification was an attempt to
    reduce ceremony/boilerplate when implementing scala-reflect macros.
    However, the unification introduces several problems:
    
    - non-standard typing rules. Consider,
        def increment(a: Int): Int = macro q"$a + 1"
      `a` is of type `Int` in the signature but type `Term` in the implementation.
    - non-standard scoping rules. Consider,
        class Number(n: Int) {
          def add(other: Int): Int = macro {
            val added = plus(other) // unresolved reference to `plus`
            q"added"
          }
          def plus(other: Int) = n + other
        }
      In this example, `add` is not a method on the class Number but instead
      a static method on a synthetic object generated by the compiler.
      This behavior has in my experience confused many in
      scalameta/paradise, and we should avoid it in the new macros.
    - inability to encode complex TypeTag dependencies. Consider,
        class Foo[A] {
          class Bar[B] {
            def mix[C] = macro impl[A, b, C] // separated impl+defn
            def mix[C] = macro {
              // unified impl+defn
              ...
            }
          }
        }
      With unification, it's not clear that we depend on the type parameters
      `A` and `B`, and we'd need some form of "hidden channel" to
      communicate that at the macro definition.
    - hijacking the compiler's syntax analyzer in order to transform the
      unified macro signature. The separated signature can be implemented
      with the standard MacroPlugin interface provided by scala-compiler.
    
    Overall, this change is a big simplification of the compiler plugin.
    The actual diff from scala-compiler is small, which increases that
    chances that it may get merged upstream to scala/scala.

--- tests/macros/src/main/scala/scala/macros/tests/Serialize.scala
+++ tests/macros/src/main/scala/scala/macros/tests/Serialize.scala
@@ -15,7 +15,10 @@ object Serialize {
     "\"" + x + "\""
   }
 
-  def materialize[T]: Serialize[T] = macro {
+  @macros.socrates
+  def materialize[T]: Serialize[T] = macro impl[T]
+  def impl[T: WeakTypeTag](c: Expansion): Term = {
+    val T = weakTypeTag[T].tpe
     val instance = Term.fresh("instance")
     val param = Term.fresh("x")
     val buf = Term.fresh("buf")
@@ -48,7 +51,7 @@ object Serialize {
       val separators = serializerss.map(
         _ => Term.Apply(Term.Select(Term.Name(buf), append), Lit.String(", ") :: Nil)
       )
-      serializerss.zip(separators).map({ case (ss, s) => ss :+ s }).flatten.dropRight(1)
+      serializerss.zip(separators).flatMap { case (ss, s) => ss :+ s }.dropRight(1)
     }
     var stats = List.newBuilder[Stat]
     stats += Defn.Val(
